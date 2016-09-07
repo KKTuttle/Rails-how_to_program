978d293d249b695fffe4807c5510b2889c003836
Katarina Tuttle and Rony Nasr <katarina+rony@>
2016-09-07 16:25:15 -0700

    add return to sections

--- spec/models/section_spec.rb
+++ spec/models/section_spec.rb
@@ -3,5 +3,12 @@ require 'rails_helper'
 describe Section do
   it {should validate_presence_of :name}
 
-  
+  # context '#next' do
+  #   it 'returns the section with the next-highest number than the current section' do
+  #     current_section = Section.create({:name => 'section1', :id => 1})
+  #     next_section = Section.create({:name => 'section2', :id => 2})
+  #     expect(current_section.next).to eq next_section
+  #   end
+  # end
+
 end
