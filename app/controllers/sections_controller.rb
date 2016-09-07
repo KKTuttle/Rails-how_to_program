304ce15334c8b3b1bce69c7bdb5976e68eb71a4c
Katarina Tuttle and Rony Nasr <katarina+rony@>
2016-09-07 16:06:52 -0700

    add working link between chapter and sections

--- app/controllers/sections_controller.rb
+++ app/controllers/sections_controller.rb
@@ -45,6 +45,6 @@ class SectionsController<ApplicationController
 
   private
     def section_params
-      params.require(:section).permit(:name)
+      params.require(:section).permit(:name, :chapter_id)
     end
 end
